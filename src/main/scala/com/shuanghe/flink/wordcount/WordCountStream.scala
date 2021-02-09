9666e1baef487b88b40832cd0b0b192774ac454f
Shuanghe Yu <shuanghe.yu@happyelements.com>
2021-02-10 00:52:12 +0800

    wordcount

--- src/main/scala/com/shuanghe/flink/wordcount/WordCountStream.scala
+++ src/main/scala/com/shuanghe/flink/wordcount/WordCountStream.scala
@@ -1,25 +1,32 @@
 package com.shuanghe.flink.wordcount
 
+import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 
 //引入隐式转换
 import org.apache.flink.streaming.api.scala._
 
 /**
- * 批处理wordcount
+ * 流式处理wordcount
  */
 object WordCountStream {
     def main(args: Array[String]): Unit = {
         val env = StreamExecutionEnvironment.getExecutionEnvironment
-        val inputDataStream: DataStream[String] = env.socketTextStream("localhost", 6666)
+        env.setParallelism(16)
+
+        val paramTool: ParameterTool = ParameterTool.fromArgs(args)
+        val host: String = paramTool.get("host")
+        val port: Int = paramTool.getInt("port")
+
+        val inputDataStream: DataStream[String] = env.socketTextStream(host, port)
 
         inputDataStream
             .flatMap(line => line.split(""))
             .filter(_.nonEmpty)
-            .map(line => (line, 1))
+            .map(line => (line, 1)).setParallelism(3)
             .keyBy(0)
-            .sum(1)
-            .print()
+            .sum(1).setParallelism(2)
+            .print().setParallelism(1)
 
         env.execute("word_count_stream")
     }
