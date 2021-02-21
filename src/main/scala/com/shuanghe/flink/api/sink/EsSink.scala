821f1dc60595f08ed394cd1b6c50f5eb4ebc5649
Shuanghe Yu <shuanghe.yu@happyelements.com>
2021-02-21 14:56:00 +0800

    window test

--- src/main/scala/com/shuanghe/flink/api/sink/EsSink.scala
+++ src/main/scala/com/shuanghe/flink/api/sink/EsSink.scala
@@ -1,8 +1,14 @@
 package com.shuanghe.flink.api.sink
 
 import com.shuanghe.flink.api.source.SensorReading
+import org.apache.flink.api.common.functions.RuntimeContext
 import org.apache.flink.streaming.api.scala._
-import org.apache.flink.streaming.connectors.elasticsearch.ElasticsearchSinkBase
+import org.apache.flink.streaming.connectors.elasticsearch.{ElasticsearchSinkFunction, RequestIndexer}
+import org.apache.flink.streaming.connectors.elasticsearch7.ElasticsearchSink
+import org.apache.http.HttpHost
+import org.elasticsearch.client.Requests
+
+import java.util
 
 object EsSink {
     def main(args: Array[String]): Unit = {
@@ -18,7 +24,29 @@ object EsSink {
                 SensorReading(arr(0), arr(1).toLong, arr(2).toDouble)
             })
 
-//        dataStream.addSink(new )
+        val httpHosts = new util.ArrayList[HttpHost]()
+        httpHosts.add(new HttpHost("localhost", 9200))
+        dataStream.addSink(new ElasticsearchSink.Builder[SensorReading](httpHosts, new MyEsSinkFunc).build())
+
         env.execute("es sink")
+    }
+}
+
+class MyEsSinkFunc extends ElasticsearchSinkFunction[SensorReading] {
+    override def process(element: SensorReading, ctx: RuntimeContext, indexer: RequestIndexer): Unit = {
+        //包装一个Map作为data source
+        val dataSource = new util.HashMap[String, String]()
+        dataSource.put("id", element.id)
+        dataSource.put("temperature", element.temperature.toString)
+        dataSource.put("timestamp", element.timestamp.toString)
+
+        //创建indexRequest,用于发送http请求
+        val indexRequest = Requests.indexRequest()
+            .index("sensor")
+            .`type`("readingdata")
+            .source(dataSource)
+
+        //用indexer发送请求
+        indexer.add(indexRequest)
     }
 }
--- src/main/scala/com/shuanghe/flink/api/window/WindowTest.scala
+++ src/main/scala/com/shuanghe/flink/api/window/WindowTest.scala
@@ -1,6 +1,7 @@
 package com.shuanghe.flink.api.window
 
-import com.shuanghe.flink.api.source.SensorReading
+import com.shuanghe.flink.api.source.{MySensorSource, SensorReading}
+import org.apache.flink.api.common.functions.ReduceFunction
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.windowing.assigners.{EventTimeSessionWindows, SlidingProcessingTimeWindows, TumblingProcessingTimeWindows}
 import org.apache.flink.streaming.api.windowing.time.Time
@@ -13,14 +14,25 @@ object WindowTest {
         val inputPath = "C:\\Users\\yushu\\studyspace\\my_flink_study\\src\\main\\resources\\sensor.txt"
         val inputStream: DataStream[String] = env.readTextFile(inputPath)
 
-        val dataStream: DataStream[SensorReading] = inputStream
-            .map(data => {
-                val arr = data.split("\t")
-                SensorReading(arr(0), arr(1).toLong, arr(2).toDouble)
-            })
+        //val dataStream: DataStream[SensorReading] = inputStream
+        //    .map(data => {
+        //        val arr = data.split("\t")
+        //        SensorReading(arr(0), arr(1).toLong, arr(2).toDouble)
+        //    })
+
+        val dataStream: DataStream[SensorReading] = env.addSource(new MySensorSource)
+
+        //dataStream.print()
 
         val resultStream = dataStream
             .keyBy(data => data.id)
+
+            /**
+             * Rather than that,if you are living in somewhere which is not using UTC±00:00 time
+             * , such as China which is using UTC+08:00,and you want a time window with size of one day
+             * , and window begins at every 00:00:00 of local time,you may use of(Time.days(1),Time.hours(-8)).
+             * The parameter of offset is Time.hours(-8)) since UTC+08:00 is 8 hours earlier than UTC time.
+             */
             //滚动时间窗口
             //.window(TumblingProcessingTimeWindows.of(Time.seconds(15), Time.seconds(3)))
             //滑动时间窗口
@@ -29,11 +41,27 @@ object WindowTest {
             //.window(EventTimeSessionWindows.withGap(Time.seconds(30)))
             //简写
             //.timeWindow(Time.seconds(15), Time.seconds(5))
+            .timeWindow(Time.seconds(15))
             //计数window
-            .countWindow(50)
-            .minBy("temperature")
-            .print()
+            //.countWindow(3)
+            //[trigger]
+            //[evictor]
+            //[allowedLateness]
+            //[sideOutputLateData]
+            //.minBy("temperature")
+            .reduce((data1, data2) => SensorReading(data1.id, data1.timestamp.max(data2.timestamp)
+                , data1.temperature.min(data2.temperature)))
+        //[getSideOutput]
 
-        env.execute("es sink")
+        resultStream.print()
+
+        env.execute("window test")
+    }
+}
+
+class MyReducer extends ReduceFunction[SensorReading] {
+    override def reduce(value1: SensorReading, value2: SensorReading): SensorReading = {
+        SensorReading(value1.id, value1.timestamp.max(value2.timestamp)
+            , value1.temperature.min(value2.temperature))
     }
 }
